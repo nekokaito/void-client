cbb1f2114ec20ca53ec9135f0e04c581e55c915b
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-11-19 23:42:37 +0600

    Root Builded

--- src/layout/root/Root.jsx
+++ src/layout/root/Root.jsx
@@ -1,9 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../../components/home/footer/Footer";
+import NavBar from "../../components/home/navbar/NavBar";
 
 
 const Root = () => {
      return (
           <div>
-               
+               <div>
+                    <NavBar></NavBar>
+                    <div className=" min-h-screen">
+                         <Outlet></Outlet>
+                    </div>
+
+                    <Footer></Footer>
+
+               </div>
           </div>
      );
 };
