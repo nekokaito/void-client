9e7dd23269dd35ae1588c36ccbb81571937ed6d9
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-11-19 23:04:37 +0600

    Firebase Initial

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
+};
+
+export const app = initializeApp(firebaseConfig);
