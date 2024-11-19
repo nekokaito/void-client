469f8a1476e77e9ad74f2863eb4cc6b490a72819
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-11-19 23:13:40 +0600

    AuthProvider: user, loading, createUser, userLogin, userLogout, googleLogin Created functions

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,10 +1,63 @@
+import { createContext, useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut,GoogleAuthProvider } from "firebase/auth";
+import { app } from "../firebase/firebase";
 
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+
+const AuthProvider = ({children}) => {
+
+     const [user, setUser] = useState(null);
+     const [loading, setLoading] = useState(true);
+
+
+
+     // Email and Password Registration 
+
+     const createUser = (email, password) => {
+          return createUserWithEmailAndPassword(auth, email, password);
+     }
+
+     // Email and Password Login and Logout
+
+     const userLogin = (email, password) => {
+          return signInWithEmailAndPassword(auth, email, password);
+     }
+
+
+     // Google Login
+     const googleLogin = () => {
+          return signInWithPopup(auth, googleProvider);
+     }
+
+
+     // User LogOut
+
+     const userLogout = () => {
+          return signOut(auth);
+     }
+
+
+     // User Auth Info
+
+     const authInfo = {
+          user,
+          loading,
+          createUser,
+          userLogin,
+          userLogout,
+          googleLogin
+     }
+
+
      return (
-          <div>
-               
-          </div>
+          <AuthContext.Provider value={authInfo}>
+               {children}
+          </AuthContext.Provider>
+          
      );
 };
 
